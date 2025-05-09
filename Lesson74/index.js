fb13df42e03c28d75eb2fc1ba4cbcca23548a531
Nik <nik_sn_fr_end@outlook.com>
2025-05-09 03:36:21 +0300

    Add files via upload

---
+++ Lesson74/index.js
@@ -0,0 +1,8 @@
+// fetch("https://api.spaceflightnewsapi.net/v4/articles?limit=5")
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log("Полученные статьи:", data);
+//   })
+//   .catch(error => {
+//     console.error("Ошибка при получении данных:", error);
+//   });
