4e9226ad9f66fcc49a37cca1ce08cffd216cb97c
Nik <nik_sn_fr_end@outlook.com>
2025-08-30 18:00:47 +0300

    add 155

---
+++ 155/script/burger.js
@@ -0,0 +1,15 @@
+const burgerButton = document.getElementById('buttonMenuOpen');
+const burgerMenu = document.getElementById('burgerMenu');
+burgerButton.addEventListener('click', function (e) {
+  if (window.innerWidth <= 900) {
+    e.preventDefault();
+    burgerMenu.classList.toggle('menu--active');
+    burgerButton.classList.toggle('nav__burger--active');
+  }
+});
+window.addEventListener('resize', function () {
+  if (window.innerWidth > 900) {
+    burgerMenu.classList.remove('menu--active');
+    burgerButton.classList.remove('nav__burger--active');
+  }
+});
